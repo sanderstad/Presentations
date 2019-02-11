86feddd568b9813edf27858a1e05af38e9524ffb
sstad <sstad@waypointanalytical.com>
2019-02-11 10:27:57 +0100

    Changed DB url Removed TLS removed WWI database actions

--- Mask That Data!/Preparations.ps1
+++ Mask That Data!/Preparations.ps1
@@ -2,11 +2,11 @@ Write-PSFMessage -Level Host -Message "Starting Preparations"
 
 $storageDir = "C:\Temp\_datamasking"
 
-$urlDDM = "https://github.com/sanderstad/Presentations/raw/master/Mask%20That%20Data!/Dynamic%20Data%20Masking/DynamicDataMasking.bak"
-$urlWWI = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak"
+$urlDb = "https://github.com/sanderstad/Presentations/raw//master/Mask%20That%20Data!/Downloads/MaskThatData.bak"
+#$urlWWI = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak"
 
-$fileDDM = "$storageDir\DynamicDataMasking.bak"
-$fileWWI = "$storageDir\WideWorldImporters-Full.bak"
+$fileDb = "$storageDir\MaskThatData.bak"
+#$fileWWI = "$storageDir\WideWorldImporters-Full.bak"
 
 $instance = "STADPC"
 
@@ -24,66 +24,31 @@ catch{
     Stop-PSFFunction -Message "Could not create directory" -Target $storageDir -ErrorRecord $_
 }
 
-# Download the DynamicDataMasking database when needed
+# Download the MaskThatData database when needed
 try{
-    if(-not (Test-Path -Path $fileDDM)){
-        Write-PSFMessage -Level Host -Message "Downloading DynamicDataMasking database"
-        Invoke-WebRequest -UseBasicParsing $urlDDM -OutFile $fileDDM
+    if(-not (Test-Path -Path $fileDb)){
+        Write-PSFMessage -Level Host -Message "Downloading MaskThatData database"
+        Invoke-WebRequest -UseBasicParsing $urlDb -OutFile $fileDb
     }
     else{
-        Write-PSFMessage -Level Verbose "DynamicDataMasking database is already present"
+        Write-PSFMessage -Level Verbose "MaskThatData database is already present"
     }
 
 }
 catch{
-    Stop-PSFFunction -Message "Could not download DynamicDataMasking database" -Target $storageDir -ErrorRecord $_
+    Stop-PSFFunction -Message "Could not download MaskThatData database" -Target $storageDir -ErrorRecord $_
 }
 
-# Download the WideWorldImporters database when needed
-try{
-    if(-not (Test-Path -Path $fileWWI)){
-        Write-PSFMessage -Level Host -Message "Downloading WideWorldImporters database"
-        Invoke-WebRequest -Uri $urlWWI -OutFile $fileWWI
-    }
-    else{
-        Write-PSFMessage -Level Verbose "WideWorldImporters database is already present"
-    }
 
+# Restore database
+try{
+    Write-PSFMessage -Level Host -Message "Restoring MaskThatData database"
+    $null = Restore-DbaDatabase -SqlInstance $instance -Path $fileDb -WithReplace
 }
 catch{
-    Stop-PSFFunction -Message "Could not download WideWorldImporters database" -Target $storageDir -ErrorRecord $_
+    Stop-PSFFunction -Message "Could not restore MaskThatData database" -Target $storageDir -ErrorRecord $_
 }
 
-# Restore DDM database
-try{
-    Write-PSFMessage -Level Host -Message "Restoring DynamicDataMasking database"
-    $null = Restore-DbaDatabase -SqlInstance $instance -Path $fileDDM -WithReplace
-}
-catch{
-    Stop-PSFFunction -Message "Could not restore DynamicDataMasking database" -Target $storageDir -ErrorRecord $_
-}
-
-# Restore the WideWorldImporters databases
-try{
-    Write-PSFMessage -Level Host -Message "Restoring WideWorldImporters database"
-    $null = Restore-DbaDatabase -SqlInstance $instance -Path $fileWWI -DatabaseName "WideWorldImporters" -ReplaceDbNameInFile -WithReplace
-
-    Write-PSFMessage -Level Host -Message "Restoring WideWorldImporters Masked database"
-    $null = Restore-DbaDatabase -SqlInstance $instance -Path $fileWWI -DatabaseName "WideWorldImporters_Masked" -ReplaceDbNameInFile -WithReplace
-}
-catch{
-    Stop-PSFFunction -Message "Could not restore WideWorldImporters database" -Target $storageDir -ErrorRecord $_
-}
-
-# Prepare the WideWorldImporters database
-try{
-    $query = "ALTER TABLE [Sales].[Customers] DROP CONSTRAINT [UQ_Sales_Customers_CustomerName]"
-
-    Invoke-DbaQuery -SqlInstance $instance -Database $databaseMasked -Query $query
-}
-catch{
-    Stop-PSFFunction -Message "Could not remove index" -Target $storageDir -ErrorRecord $_
-}
 
 
 
