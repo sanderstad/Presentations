0e99d4f9de5c40e839f5437dc9b14dab24705c8c
sqlstad <sanderstad@gmail.com>
2020-03-03 10:12:36 +0100

    Fixed build script

--- Solving the dev DB problem with Docker and dbaclone/Scripts/04.Build.ps1
+++ Solving the dev DB problem with Docker and dbaclone/Scripts/04.Build.ps1
@@ -1,1 +1,8 @@
-. "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild" .\SSDT\StackOverflow2013\StackOverflow2013.sln
+# Setting environment varibles
+$msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+
+$projectFile = "C:\Users\sstad\source\repos\Databases\StackOverflow2013\StackOverflow2013-Tests\StackOverflow2013-Tests.sqlproj"
+
+# Running build
+.  $msbuildPath $projectFile
+
