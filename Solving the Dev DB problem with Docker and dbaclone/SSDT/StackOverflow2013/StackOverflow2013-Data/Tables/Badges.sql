abf2ceadbeb701ddc295350862266d60c1186941
sqlstad <sanderstad@gmail.com>
2020-03-03 22:09:03 +0100

    Added presentation

---
+++ Solving the Dev DB problem with Docker and dbaclone/SSDT/StackOverflow2013/StackOverflow2013-Data/Tables/Badges.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Badges] (
+    [Id]     INT           IDENTITY (1, 1) NOT NULL,
+    [Name]   NVARCHAR (40) NOT NULL,
+    [UserId] INT           NOT NULL,
+    [Date]   DATETIME      NOT NULL,
+    CONSTRAINT [PK_Badges_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Solving the Dev DB problem with Docker and dbaclone/SSDT/StackOverflow2013/StackOverflow2013-Data/Tables/LinkTypes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[LinkTypes] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Type] VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_LinkTypes_Id] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
