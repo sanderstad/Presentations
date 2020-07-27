972c29ecf7af21272e6a3b8dec1658183562ae50
sqlstad <sanderstad@gmail.com>
2020-07-27 15:34:34 +0200

    Improved preliminary scripts

--- Solving the Dev DB problem with Docker and dbaclone/Scripts/00_CreateSSDTProject.ps1
+++ Solving the Dev DB problem with Docker and dbaclone/Scripts/00_CreateSSDTProject.ps1
@@ -1,66 +1,16 @@
-[CmdLetbinding()]
-
 # URL for SSDT project template
-# "https://github.com/sanderstad/SSDT-With-tSQLt-Template"
+# https://github.com/sanderstad/SSDT-With-tSQLt-Template
 
-# EXAMPLE
-# .\00_CreateSSDTProject.ps1 -ProjectName StackOverflow2013 -OutputPath C:\projects\ -TemplateName SSDT-With-tSQLt-Template
-# Create a SSDT solution for a database called "DB1" and output it to "C:\Projects"
+# Import the global variables
+. ".\variables.ps1"
 
 
 ########################################################################################
 # DON'T CHANGE ANYTHING BELOW                                                          #
 ########################################################################################
-
-param(
-    [string]$ProjectName,
-    [string]$OutputPath,
-    [string]$TemplateName,
-    [string]$TemplateDescription,
-    [switch]$RemoveExistingProject,
-    [switch]$RemoveExistingTemplate
-)
-
-if (-not (Get-Module -ListAvailable -Name 'dbatools')) {
-    Write-Warning "Please install the dbatools PowerShell module. 'Install-Module dbatools'"
+if ((Get-Module -ListAvailable).Name -notcontains 'PSFrameWork') {
+    Write-Warning "Please install PSFramework using 'Install-Module PSFramework'"
     return
-}
-
-if (-not (Get-Module -ListAvailable -Name 'PSFrameWork')) {
-    Write-Warning "Please install the PSFrameWork PowerShell module. 'Install-Module PSFrameWork'"
-    return
-}
-
-if (-not $ProjectName) {
-    Stop-PSFFunction -Message "Please enter a project name" -Target $SqlInstance -Continue
-}
-
-if (-not $OutputPath) {
-    Stop-PSFFunction -Message "Please enter an output path" -Target $OutputPath -Continue
-}
-else {
-    if (-not (Test-Path -Path $OutputPath)) {
-        try {
-            $null = New-Item -Path $OutputPath -ItemType Directory
-        }
-        catch {
-            Stop-PSFFunction -Message "Could not create output path directory" -Target $OutputPath -ErrorRecord $_
-        }
-    }
-}
-
-if (-not $RemoveExistingProject -and (Get-ChildItem -Path (Join-Path -Path $OutputPath -ChildPath $ProjectName) -ErrorAction SilentlyContinue)) {
-    Stop-PSFFunction -Message "There is already a project with files present. Please use -RemoveExistingProject automatically remove it"
-}
-
-if (Test-PSFFunctionInterrupt) { return }
-
-if (-not $TemplateName) {
-    $TemplateName = "SSDT-With-tSQLt"
-}
-
-if (-not $TemplateDescription) {
-    $TemplateDescription = "SSDT project template including tSQLt"
 }
 
 # Setup variables
@@ -69,42 +19,36 @@ $zipFilePath = "$($Env:TEMP)\SSDT-With-tSQLt-Template.zip"
 $archiveDestPath = "$($Env:TEMP)\SSDT-With-tSQLt-Template"
 $pathToTemplate = "$($archiveDestPath)\SSDT-With-tSQLt-Template-master"
 
-
-# Setup the full project path
-$projectPath = Join-Path -Path $OutputPath -ChildPath $ProjectName
-
-if ($RemoveExistingProject) {
-    if ((Test-Path -Path $projectPath)) {
-        try {
-            Write-PSFMessage -Level Host -Message "Removing project destination path '$projectPath'"
-            Remove-Item -Path $projectPath -Recurse -Force
-        }
-        catch {
-            Stop-PSFFunction -Message "Could not remove project directory" -ErrorRecord $_ -Target $projectPath -Continue
-        }
+# Check if the template is not already there
+$templates = Get-PSMDTemplate # Should not contain 'SSDT-With-tSQLt-Template'
+if ($templates.Name -contains $templateName) {
+    try {
+        Write-PSFMessage -Level Host -Message "Removing PSF template"
+        Remove-PSMDTemplate -TemplateName $templateName -Confirm:$false
+    }
+    catch {
+        Stop-PSFFunction -Message "Could not remove template"
+        return
     }
 }
 
-# Check if the template is not already there
-$templates = Get-PSMDTemplate # Should not contain 'SSDT-With-tSQLt-Template'
-if ($RemoveExistingTemplate) {
-    if ($templates.Name -contains $TemplateName) {
-        try {
-            Write-PSFMessage -Level Host -Message "Removing PSF template"
-            Remove-PSMDTemplate -TemplateName $TemplateName -Confirm:$false
-        }
-        catch {
-            Stop-PSFFunction -Message "Could not remove template"
-            return
-        }
+# Check if the directory is already there
+$projectPath = Join-Path -Path $projectDestinationPath -ChildPath $projectName
+if ((Test-Path -Path $projectDestinationPath)) {
+    try {
+        Write-PSFMessage -Level Host -Message "Removing project destination path '$projectDestinationPath'"
+        $null = Remove-Item -Path $projectDestinationPath -Recurse -Force
+    }
+    catch {
+
     }
 }
 
 # Remove the template directory
 if (Test-Path -Path $archiveDestPath) {
     try {
-        Write-PSFMessage -Level Host -Message "Removing existing archive destination path '$archiveDestPath'"
-        Remove-Item -Path $archiveDestPath -Recurse -Force
+        Write-PSFMessage -Level Host -Message "Removing existing archive destination path 'v'"
+        $null = Remove-Item -Path $archiveDestPath -Recurse -Force
     }
     catch {
         Stop-PSFFunction -Message "Could not remove archive destination directory '$archiveDestPath'"
@@ -112,15 +56,13 @@ if (Test-Path -Path $archiveDestPath) {
 }
 
 # Create the project dir
-if (-not (Test-Path -Path $projectPath)) {
-    try {
-        Write-PSFMessage -Level Host -Message "Creating project directory '$projectPath'"
-        $null = New-Item -Path $projectPath -ItemType Directory
-    }
-    catch {
-        Stop-PSFFunction -Message "Could not create project destination directory"
-        return
-    }
+try {
+    Write-PSFMessage -Level Host -Message "Creating project directory '$projectPath'"
+    $null = New-Item -Path $projectPath -ItemType Directory
+}
+catch {
+    Stop-PSFFunction -Message "Could not create project destination directory"
+    return
 }
 
 # Download the file
@@ -145,8 +87,8 @@ catch {
 
 # Create the template
 try {
-    Write-PSFMessage -Level Host -Message "Creating new PSF template '$TemplateName' from '$pathToTemplate'"
-    New-PSMDTemplate -ReferencePath $pathToTemplate -TemplateName $TemplateName -Description $TemplateDescription -Force
+    Write-PSFMessage -Level Host -Message "Creating new PSF template '$templateName' from '$pathToTemplate'"
+    New-PSMDTemplate -ReferencePath $pathToTemplate -TemplateName $templateName -Description $templateDescription -Force
 }
 catch {
     Stop-PSFFunction -Message "Something went wrong creating the template" -Target $url -ErrorRecord $_
@@ -155,8 +97,8 @@ catch {
 
 # Create the project
 try {
-    Write-PSFMessage -Level Host -Message "Creating solution from template '$TemplateName'"
-    Invoke-PSMDTemplate -TemplateName $TemplateName -OutPath $OutputPath -Name $ProjectName -Force
+    Write-PSFMessage -Level Host -Message "Creating solution from template '$templateName'"
+    Invoke-PSMDTemplate -TemplateName $templateName -OutPath $projectDestinationPath -Name $projectName -Force
 }
 catch {
     Stop-PSFFunction -Message "Something went wrong creating the project" -Target $url -ErrorRecord $_
@@ -164,4 +106,4 @@ catch {
 }
 
 # Open the windows explorer with this solution
-explorer ($projectPath)
+explorer (Join-Path -Path $projectDestinationPath -ChildPath $projectName)
--- Solving the Dev DB problem with Docker and dbaclone/Scripts/00_CreateUnitTests.ps1
+++ Solving the Dev DB problem with Docker and dbaclone/Scripts/00_CreateUnitTests.ps1
@@ -1,61 +1,16 @@
-[CmdLetbinding()]
+# URL for PowerShell module
+# https://github.com/sanderstad/PStSQLtTestGenerator
 
-# URL for SSDT project template
-# "https://github.com/sanderstad/SSDT-With-tSQLt-Template"
+. ".\variables.ps1"
 
-# EXAMPLE
-# .\00_CreateUnitTests.ps1 -SqlInstance localhost -Database StackOverflow2013 -OutputPath "C:\projects\StackOverflow2013\StackOverflow2013-Tests\TestBasic"
-# Get all the objects from database "DB1" and create the basic unit tests to the
-# basic test folder of the test project within solution created with the SSDT-With-tSQLt-Template
-
-param(
-    [string]$SqlInstance,
-    [pscredential]$SqlCredential,
-    [string]$Database,
-    [string]$OutputPath
-)
+#$cred = Get-Credential
 
 ########################################################################################
 # DON'T CHANGE ANYTHING BELOW                                                          #
 ########################################################################################
-
-if (-not (Get-Module -ListAvailable -Name 'dbatools')) {
-    Write-Warning "Please install the dbatools PowerShell module. 'Install-Module dbatools'"
-    return
-}
-
-if (-not (Get-Module -ListAvailable -Name 'PSFrameWork')) {
-    Write-Warning "Please install the PSFrameWork PowerShell module. 'Install-Module PSFrameWork'"
-    return
-}
-
-if (-not (Get-Module -ListAvailable -Name 'PStSQLtTestGenerator')) {
+if ((Get-Module -ListAvailable).Name -notcontains 'PStSQLtTestGenerator') {
     Write-Warning "Please install PStSQLtTestGenerator using 'Install-Module PStSQLtTestGenerator'"
     return
 }
 
-if (-not $SqlInstance) {
-    Stop-PSFFunction -Message "Please enter a sql server instance" -Continue
-}
-
-$server = Connect-DbaInstance -SqlInstance $SqlInstance -SqlCredential $SqlCredential
-
-if (-not $Database) {
-    Stop-PSFFunction -Message "Please enter a database" -Continue
-}
-else {
-    if ($server.Databases.Name -notcontains $Database) {
-        Stop-PSFFunction -Message "Database [$($database)] does not exists on $($server.DomainInstanceName)" -Continue
-    }
-}
-
-if (-not $OutputPath) {
-    Stop-PSFFunction -Message "Please enter an output path" -Continue
-}
-else {
-    if (-not (Test-Path -Path $OutputPath)) {
-        Stop-PSFFunction -Message "Output path '$($OutputPath)' does not exist" -Target $OutputPath -Continue
-    }
-}
-
-Invoke-PSTGTestGenerator -SqlInstance $SqlInstance -SqlCredential $SqlCredential -Database $Database -OutputPath $OutputPath
+Invoke-PSTGTestGenerator -SqlInstance $instance -SqlCredential $cred -Database $database -OutputPath $destination
