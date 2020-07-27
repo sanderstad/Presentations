d5f2736b5fbbbf96afae463b11f34fb8ab6b4848
sqlstad <sanderstad@gmail.com>
2020-07-27 15:35:05 +0200

    Initial commit

---
+++ Solving the Dev DB problem with Docker and dbaclone/Scripts/variables.ps1
@@ -0,0 +1,16 @@
+# SSDT project creation
+$projectName = "StackOverflow2013"
+$templateName = "SSDT-With-tSQLt"
+$templateDescription = "SSDT project template including tSQLt"
+$projectDestinationPath = "c:\temp\SolvingDevDBProblem\"
+
+# Database and object creation
+$instance = "localhost"
+$database = "StackOverflow2013"
+
+<# $username = "username"
+$password = ConvertTo-SecureString -String "password" -AsPlainText -Force
+$cred = New-Object -typename System.Management.Automation.PSCredential -argumentlist $username, $password #>
+
+# Unit testing variables
+$destination = Join-Path -Path $projectDestinationPath -ChildPath "$projectName\$($projectName)-Tests\TestBasic\"
