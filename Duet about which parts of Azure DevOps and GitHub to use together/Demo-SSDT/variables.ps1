2c507a5d0ea4c22b3ed5470811c9efa611300a51
Sander Stad <sanderstad@gmail.com>
2021-10-24 17:05:58 +0200

    added sessions

---
+++ Duet about which parts of Azure DevOps and GitHub to use together/Demo-SSDT/variables.ps1
@@ -0,0 +1,18 @@
+# SSDT project creation
+$projectName = "UnitTesting"
+$templateName = "SSDT-With-tSQLt"
+$templateDescription = "SSDT project template including tSQLt"
+$projectDestinationPath = "c:\temp\DevOpsDuet\"
+
+# Database and object creation
+$instance = "localhost"
+$database = "UnitTesting"
+$tableCount = 10
+$maxColumns = 20
+
+$username = "sa"
+$password = ConvertTo-SecureString -String "MyStr0ngP@ssword" -AsPlainText -Force
+$cred = New-Object -typename System.Management.Automation.PSCredential -argumentlist $username, $password #>
+
+# Unit testing variables
+$destination = Join-Path -Path $projectDestinationPath -ChildPath "$projectName\$($projectName)-Tests\TestBasic\"
